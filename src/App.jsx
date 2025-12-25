41b9e9f6e04b1f3904cc9440a0fb9b3d37067be4
cozy-bit <cozybit.dev@gmail.com>
2025-12-25 17:30:13 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+import { Linter } from 'eslint'
+
+function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (inputValue.trim() === '') return
+    const newTodo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false,
+    }
+
+    setTodos([...todos, newTodo])
+    setInputValue('')
+  }
+
+  return (
+    <div className="app">
+      <h1>Мои задачи</h1>
+      <p className="counter">Всего задач: 0</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder='Добавить новую задачу'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
+        <button type="submit">Добавить</button>
+      </form>
+
+      <ul className="todo-lsit">
+        {todos.map(todo => (<li key={todo.id}><span>{todo.text}</span></li>))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
